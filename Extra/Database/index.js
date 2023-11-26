8e637ab7d6521bc0b27bd028db6deb291b4b0a4a
Ph Minh Dong <142565609+dongdev06@users.noreply.github.com>
2023-11-26 15:36:59 +0700

    Update index.js

--- Extra/Database/index.js
+++ Extra/Database/index.js
@@ -7,11 +7,11 @@ const fs = require('fs-extra');
 const request = require('request');
 const deasync = require('deasync');
 
-if (!fs.existsSync(process.cwd() + '/Horizon_Database')) {
-    fs.mkdirSync(process.cwd() + '/Horizon_Database');
-    fs.writeFileSync(process.cwd() + '/Horizon_Database/A_README.md', 'This folder is used by ChernobyL(NANI =)) ) to store data. Do not delete this folder or any of the files in it.', 'utf8');
+if (!fs.existsSync(process.cwd() + '/Data_MDong')) {
+    fs.mkdirSync(process.cwd() + '/Data_MDong');
+    fs.writeFileSync(process.cwd() + '/Data_MDong/A_README.md', 'This folder is used by ChernobyL(NANI =)) ) to store data. Do not delete this folder or any of the files in it.', 'utf8');
 }
-var db = new BetterDB(process.cwd() + "/Horizon_Database/SyntheticDatabase.sqlite");
+var db = new BetterDB(process.cwd() + "/Data_MDong/SyntheticDatabase.sqlite");
 
 function Lset(key, value) {
     if (!key)
